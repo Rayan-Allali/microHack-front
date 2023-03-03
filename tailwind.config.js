81eb79618ded5c73d68b93d5879aaf3332a7dcc8
MoncefME <ka_moussaoui@esi.dz>
2023-03-03 16:02:56 +0100

    working on the login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",],
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "auth-bg": "#F3F1EF",
+        "auth-btn": "#4BA064",
+      },
+    },
   },
   plugins: [],
-}
+};
