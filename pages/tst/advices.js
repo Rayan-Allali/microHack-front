664a75370ee873971ca8b7b7f1d8ebc9f57abc76
Ayyoub Kasmi <ayyoubkasmidz@gmail.com>
2023-03-04 02:32:42 +0100

    finished 2nd page+ created & edited components
    Created the Row and HeaderRow components to implement the advices and list-of-farmers pages.

--- pages/tst/advices.js
+++ pages/tst/advices.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import Product from '../../Components/Product/index'
+import HeaderRow from '../../Components/HeaderRow'
+import Row from '../../Components/Row/index'
 
 function advices() {
   return (
     <main className='p-16 w-full'>
       
-      <div className='pb-12'>
+      <div className='pb-12 mb-4'>
 
         <h1 className=' text-5xl font-bold pb-4'>The Most Needed Products</h1>
         <p className=' text-s'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quibusdam ullam.
@@ -14,18 +15,18 @@ function advices() {
       
       </div>
       
-      <div className='w-full h-10 bg-white flex justify-between items-center py-2 px-6 mb-4 border-solid border-2 border-gray-300'>
-
-        <div className='w-20 text-center'>Product</div>
-        <div className='w-20 text-center'>Wilaya</div>
-        <div className='w-20 text-right'>Period</div>
-
-      </div>
+      <HeaderRow title1="Product" title2="Wilaya" title3="Period" />
 
       <div className='flex flex-col justify-between'>
-        <Product/>
-        <Product/>
-        <Product/>
+      
+        <Row imageSrc="/Assets/potato.png" rounded={false} width={72} height={52} info1="Potatos" info2="Alger" info3="Fevrier" />
+        <Row imageSrc="/Assets/potato.png" rounded={false} width={72} height={52} info1="Potatos" info2="Alger" info3="Fevrier" />
+        <Row imageSrc="/Assets/potato.png" rounded={false} width={72} height={52} info1="Potatos" info2="Alger" info3="Fevrier" />
+        <Row imageSrc="/Assets/potato.png" rounded={false} width={72} height={52} info1="Potatos" info2="Alger" info3="Fevrier" />
+        <Row imageSrc="/Assets/potato.png" rounded={false} width={72} height={52} info1="Potatos" info2="Alger" info3="Fevrier" />
+        <Row imageSrc="/Assets/potato.png" rounded={false} width={72} height={52} info1="Potatos" info2="Alger" info3="Fevrier" />
+        <Row imageSrc="/Assets/potato.png" rounded={false} width={72} height={52} info1="Potatos" info2="Alger" info3="Fevrier" />
+      
       </div>
       
     </main>
